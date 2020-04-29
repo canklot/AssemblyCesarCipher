5ea6e89788dd8171d4ae9241d12462693e6009b2
Canklot <mustafacandan211@gmail.com>
2020-04-29 18:08:15 +0300

    Double digit input added and jump condition changed

--- AssemblyCesarCipher.asm
+++ AssemblyCesarCipher.asm
@@ -5,7 +5,9 @@
 CR EQU 0DH
 LF EQU 0AH
 
-Numbers DB ?,'$'        ;Soru isareti initialize olmamis tek byte demek max 127 tutabilir. Dolar isareti ise string sonu demek.
+numbers DB ?,'$'        ;Soru isareti initialize olmamis tek byte demek max 127 tutabilir. Dolar isareti ise string sonu demek.
+
+harf DB ?
 
 MSG0 DB 'WELCOME TO THE EMU8086 CESAR CIPHER: ',0DH,0AH,0DH,0AH, 'PLEASE ENTER HOW MANY TIMES DO YOU WANT TO SHIFT THE ALPHABET:  ', '$'
 MSG1 DB 0DH,0AH, 0DH,0AH, 'ENTER THE ORIGINAL MESSAGE LETTER BY LETTER: $'
@@ -21,21 +23,7 @@ MAIN PROC
     MOV AX, @DATA       ;get data segment
     MOV DS,AX           ;initailize DS
     
-    ;numbers test
-    MOV AH,1            ;AH registerine 1 yaz.
-    INT 21H             ;AH registerine bak ve icindeki sayiya karsilik gelen fonksiyonu cagir. INT21,1 fonksiyonu klavyeden veri girisi icin beklenir. Klavyeden girilen tus AL yazmacina koyulur.
-    SUB AL,30h          ;1'in ascii degeri hexadecimal 31 oldugu icin 30h cikariyoruz.
-    MOV DL,10h
-    MUL DL
-    MOV Numbers,AL 
     
-    
-     
-    MOV AH,1            ;AH registerine 1 yaz.
-    INT 21H             ;AH registerine bak ve icindeki sayiya karsilik gelen fonksiyonu cagir. INT21,1 fonksiyonu klavyeden veri girisi icin beklenir. Klavyeden girilen tus AL yazmacina koyulur.
-    SUB AL,30h
-    ADD AL,Numbers
-    MOV Numbers,AL
     
     
     ;display welcome message
@@ -43,11 +31,19 @@ MAIN PROC
     MOV AH,9            ;AH registerina 9 yaz. Bu ekrana bas demek
     INT 21H             ;AH registerine bak ve icindeki sayiya karsilik gelen INT21 fonksiyonu'nu cagir. Bu fonksiyon dx yazmacinda belirtilen ofset adresinden itibaren $ karakteri ile karsilasana kadar karakterleri ekrana yazar.
   
-    ;input number
+    ;input numbers
     MOV AH,1            ;AH registerine 1 yaz.
     INT 21H             ;AH registerine bak ve icindeki sayiya karsilik gelen fonksiyonu cagir. INT21,1 fonksiyonu klavyeden veri girisi icin beklenir. Klavyeden girilen tus AL yazmacina koyulur.
-    SUB AL,30h          ;Neden bilmiyorum ama 1 girince 31 oluyor o yuzden 30 cikardik. Normal 30 yazinca olmuyor 30h olmasi lazim. Debug yaparken variables kisminda degiskenleri hex,bin,ascii olarak goster var o zaman anladim. Assci olarak 1 hexadecimal 31 oluyorus. Cok sacma ben 1'in degeri 1 olmali bence neden gidip taa 31'e koyarsin ki?
-    MOV BH,AL           ;AL'yi BH'a kopyala
+    SUB AL,30h          ;1'in ascii degeri hexadecimal 31 oldugu icin 30h cikariyoruz.
+    MOV DL,10
+    MUL DL
+    MOV numbers,AL
+    
+    MOV AH,1            ;AH registerine 1 yaz.
+    INT 21H             ;AH registerine bak ve icindeki sayiya karsilik gelen fonksiyonu cagir. INT21,1 fonksiyonu klavyeden veri girisi icin beklenir. Klavyeden girilen tus AL yazmacina koyulur.
+    SUB AL,30h
+    ADD AL,numbers
+    MOV numbers,AL
  
     
 
@@ -55,7 +51,7 @@ MAIN PROC
     go:                 ;go isimli bir belirtec koy
     
     ;display MSG1
-    LEA DX,MSG1         ;MSG0'in adresini dx'e yukle
+    LEA DX,MSG1         ;MSG1'in adresini dx'e yukle
     MOV AH,9            ;AH registerina 9 yaz.
     INT 21H             ;AH registerine bak ve icindeki sayiya karsilik gelen INT21 fonksiyonu'nu cagir. Bu fonksiyon dx yazmacinda belirtilen ofset adresinden itibaren $ karakteri ile karsilasana kadar karakterleri ekrana yazar.
 
@@ -67,12 +63,18 @@ MAIN PROC
     ;input letter
     MOV AH,1            ;AH registerine 1 yaz
     INT 21H             ;AH registerine bak ve icindeki sayiya karsilik gelen fonksiyonu cagir. INT21,1 fonksiyonu klavyeden veri girisi icin beklenir. Klavyeden girilen tus AL yazmacina koyulur.
-    add AL, bh          ;AL registerini BH kadar arttir
+    MOV harf, AL
+    
+   
+    
+    ;Add
+    MOV AL, harf
+    add AL, numbers          ;AL registerini arttir
+    mov harf, AL
     
     
-    
-    CMP AL,0x79
-    JG  alphabetoverflow
+    CMP AL,7Ah
+    JNB  alphabetoverflow
      
     displayciphered: 
      
@@ -104,3 +106,7 @@ MAIN PROC
     
     ;not tek basamakli sayi girilebiliyor sadece onu duzelt
     
+    
+    ;Not tum sayilari decimale cevir oyle islem yap hex oldugunu unutuyorsun sonra 1+10 neden 17 yapiyor diyosun
+    
+    ;Neden bilmiyorum ama 1 girince 31 oluyor o yuzden 30 cikardik. Normal 30 yazinca olmuyor 30h olmasi lazim. Debug yaparken variables kisminda degiskenleri hex,bin,ascii olarak goster var o zaman anladim. Assci olarak 1 hexadecimal 31 oluyorus. Cok sacma ben 1'in degeri 1 olmali bence neden gidip taa 31'e koyarsin ki?
